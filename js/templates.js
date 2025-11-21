51f86eb8e021af0207850d78ac219f72556e87af
obenning <62991587+obenning@users.noreply.github.com>
2025-11-21 11:44:09 +0100

    Update templates.js

--- js/templates.js
+++ js/templates.js
@@ -1807,7 +1807,7 @@
                                     <div style="font-family: 'Font Awesome 5 Pro'; font-size: {{iconSizeType}}; color: {{iconColor}}; display: flex; align-items: center; justify-content: center; width: {{iconContainerSizeType}}; height: {{iconContainerSizeType}}; background: {{iconBackground}}; border-radius: {{iconRadiusType}};">{{iconClass}}</div>
                                 </div>
                                 <div style="margin-bottom: {{titleSpacing}};">
-                                    <h3 style="font-family: var(--heading-font-font-family); font-size: {{titleSizeType}}; font-weight: var(--heading-font-font-weight); line-height: var(--heading-font-line-height); color: {{titleColor}}; margin: 0; text-align: left;">{{title}}</h3>
+                                    <h3 style="font-family: var(--heading-font-font-family); font-size: {{titleSizeType}}; font-weight: var(--heading-font-font-weight); line-height: var(--heading-font-line-height); color: {{titleColor}}; margin: 0; text-align: {{titleAlignment}};">{{title}}</h3>
                                 </div>
                                 <div style="margin-bottom: {{textSpacing}};">
                                     <p style="font-family: var(--body-font-font-family); font-size: {{textSizeType}}; line-height: var(--body-font-line-height); color: {{textColor}}; margin: 0;">{{text}}</p>
@@ -1825,6 +1825,7 @@
                 "properties": {
                     // === CONTENT ===
                     "title": "Professionelle Compliance-Lösungen",
+                    "titleAlignment": "left",
                     "titleColor": "#063AA8",
                     "titleSizeType": "large",
                     "titleSpacing": "1rem",
